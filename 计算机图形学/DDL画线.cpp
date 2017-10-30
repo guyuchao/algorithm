1c6377c47bf305d412cae125ddfb11b13127e6ae
guyuchao <2595847310@qq.com>
2017-10-30 19:33:42 +0800

    submit

---
+++ 计算机图形学/DDL画线.cpp
@@ -0,0 +1,54 @@
+#include <GL/glut.h>  
+#include<iostream>
+using namespace std;
+
+int width = 200;
+int height = 200;
+
+unsigned char* pixels;
+
+
+
+void DDL_init(int x1,int y1,int x2,int y2){
+	int index;
+	double k = (y2 - y1) *1.0/ (x2 - x1);
+	double now_y = y1;
+	for (int x = x1; x <= x2; x++) {
+		int y = int(now_y + 0.5);
+		//cout << x << " " << y << endl;
+		index = y*width + x;
+		pixels[index * 3 + 0] = 255;
+		pixels[index * 3 + 1] = 0;
+		pixels[index * 3 + 2] = 0;
+		now_y += k;
+	}
+
+}
+void mydraw() {
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glDrawPixels(width, height, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+	glFlush();
+}
+
+
+int main(int argc, char *argv[])
+{
+	
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+	glutInitWindowPosition(300, 100);//
+	glutInitWindowSize(width, height);
+	glutCreateWindow("DDL画线");
+
+	
+	pixels = new unsigned char[width*height * 3];
+	int x1, y1, x2, y2;
+	cout << "请输入两个点坐标：  ";
+	cin >> x1 >> y1 >> x2 >> y2;
+	DDL_init(x1,y1,x2,y2);
+	glutDisplayFunc(&mydraw);		
+	
+	glutMainLoop();
+	return 0;
+}
---
+++ 链表递归栈/矩阵快速幂位运算.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+class matrix {
+public:
+	int x11, x12;
+	int x21, x22;
+	matrix(int tmp_x11, int tmp_x12, int tmp_x21, int tmp_x22) {
+		this->x11 = tmp_x11;
+		this->x12 = tmp_x12;
+		this->x21 = tmp_x21;
+		this->x22 = tmp_x22;
+	}
+	matrix() {
+		this->x11 = 1;
+		this->x12 = 0;
+		this->x21 = 0;
+		this->x22 = 1;
+	}
+	matrix operator *(const matrix b) {
+		int tmp_x11 = this->x11*b.x11 + this->x12*b.x21;
+		int tmp_x12 = this->x11*b.x12 + this->x12*b.x22;
+		int tmp_x21 = this->x21*b.x11 + this->x22*b.x12;
+		int tmp_x22 = this->x21*b.x12 + this->x22*b.x22;
+		return matrix(tmp_x11, tmp_x12, tmp_x21, tmp_x22);
+	}
+	void print() {
+		cout << x11 << "   " << x12 << endl;
+		cout << x21 << "   " << x22;
+	}
+};
+
+matrix quick_pow(matrix a,int times) {
+	if (times == 1)return a;
+	if (times % 2 == 0) {
+		return quick_pow(a,times/2)*quick_pow(a, times / 2);
+	}
+	else {
+		return quick_pow(a, (times-1) / 2)*quick_pow(a, (times-1) / 2)*a;
+	}
+}
+
+int main() {
+	matrix x(0, 1, 1, 1);
+	int a1 = 1, a2 = 1;
+	int n = 20;
+	n = n - 2;
+	matrix b;
+	while (n > 0) {
+		if (n & 1) {
+			b = b*x;
+		}
+		x = x*x;
+		n=n >> 1;
+	}
+	//b.print();
+	//matrix b = quick_pow(x, n-2);
+	cout << b.x21*a1 + b.x22*a2;
+
+
+	system("pause");
+	return 0;
+}
